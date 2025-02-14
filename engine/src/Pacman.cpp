fae7f9d20cfd368ed8e3dbb4f2bf5e0f3a39c318
daniwaked <dani.waked.0001@student.uu.se>
2025-02-14 11:04:33 +0100

    setting up Pacman class header file

---
+++ engine/src/Pacman.cpp
@@ -0,0 +1,32 @@
+#include "Pacman.h"
+
+Pacman::Pacman()
+{
+
+}
+
+void Pacman::Update()
+{
+
+    Movement();
+}
+
+void Pacman::Draw()
+{
+    
+}
+
+Direction Pacman::GetMoveDirection()
+{
+    return Direction();
+}
+
+void Pacman::SetMoveDirection(Direction p_direction)
+{
+
+}
+
+void Pacman::Movement()
+{
+
+}
