f17955a576019aa64161902b0e3045f7b0a0a0d0
daniwaked <dani.waked.0001@student.uu.se>
2025-02-27 14:21:53 +0100

    added sound files

---
+++ engine/src/SoundManager.cpp
@@ -0,0 +1,21 @@
+#include "SoundManager.h"
+
+SoundManager::SoundManager()
+{
+}
+
+SoundManager::~SoundManager()
+{
+}
+
+void SoundManager::PlaySound()
+{
+}
+
+void SoundManager::SetVolume()
+{
+}
+
+void SoundManager::StopSound()
+{
+}
